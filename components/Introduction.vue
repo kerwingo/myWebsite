08c0448de040c2b1e2d8f0fca859b0bf71824951
kerwin <7441437+kerwingo@user.noreply.gitee.com>
2022-12-20 19:10:12 +0800

    update

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="about padding section">
 <p class="boldText"><b>Hi, I’m Kerwin 👋</b><br>Self taught Full-stack web developer.</p>
-<p class="mt-4 normalText opacity-60">I'm currently looking at opportunities for front-end engineers.</p>
+<p class="mt-4 normalText opacity-60">I'm looking for a job as a front-end engineer.</p>
 <p class="mt-4 normalText opacity-60">I’m a 28 years old Self taught Full-stack web developer in <a href="https://www.bing.com/search?q=Chengdu, Sichuan" target="_blank" class="border-b-2 border-gray-500 border-opacity-40 hover:border-opacity-70">Chengdu, Sichuan</a>, I usually make and share open-source projects. Also share all my open-source projects on GitHub.</p>
 <a href="/about" class="mt-4 mr-2 rounded-full generalBtn"><p class="p-3 generalBtnText smallText opacity-80">About</p></a>
 </div>
