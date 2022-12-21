cb5e82e082dc3327281cf9e02747b6a1e12b8501
kerwin <kerwin_1727@163.com>
2022-12-22 00:25:06 +0800

    update

--- components/ImageWp.vue
+++ components/ImageWp.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="image section">
-<img class="ImageWp" lazy="true" src="https://s1.ax1x.com/2022/12/20/zLtfTP.jpg" draggable="false">
+<img class="ImageWp" lazy src="https://s1.ax1x.com/2022/12/20/zLtfTP.jpg" draggable="false">
 <p class="mt-4 text-center smallText opacity-60">Chengdu, Sichuan</p>
 </div>
 </template>
